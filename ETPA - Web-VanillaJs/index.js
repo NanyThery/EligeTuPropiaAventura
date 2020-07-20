a1e1921e018f7f68d423925b565e15fd8d3d6a9e
Nany Thery <nadinethery@gmail.com>
2020-07-20 23:13:56 +0200

    Initial Html interface, js, and main object story

---
+++ ETPA - Web-VanillaJs/index.js
@@ -0,0 +1,4 @@
+//Hacer función para insertar el texto inicial
+//Hacer función para coja la opción del input, y la inserte como un paragraph. 
+// y con esa opción busque el output en el json y con ese output, llame al texto o textos correspondientes y los inserte en un nuevo child. 
+
