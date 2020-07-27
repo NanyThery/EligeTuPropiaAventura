00468e57e38d7b396be6c6e275f6c0dc4ec51c31
Nany Thery <nadinethery@gmail.com>
2020-07-27 17:00:08 +0200

    Correct some misspelling

--- ETPA - Web-VanillaJs/index.js
+++ ETPA - Web-VanillaJs/index.js
@@ -1,7 +1,3 @@
-//Hacer función para insertar el texto inicial
-//Hacer función para coja la opción del input, y la inserte como un paragraph. 
-// y con esa opción busque el output en el json y con ese output, llame al texto o textos correspondientes y los inserte en un nuevo child. 
-
 let story;
 
 fetch("./storyText.json")
